ec78c2a4311c24399a4a5417af5ec5524c120185
Oleksii Shevchenko <shevaroller@gmail.com>
2015-09-13 22:35:51 -0600

    return true on success, add a test for that

--- test/test.js
+++ test/test.js
@@ -1,8 +1,4 @@
 var patch  = require('../lib/cordova-auto-patch');
 var assert = require('assert');
 
-assert.equal(
-  patch('./test/'), 
-  'build version: 1.0.2',
-  'increment patch version in config.xml'
-  );
+assert.equal(patch('./test/'), true, 'return true on completion');
